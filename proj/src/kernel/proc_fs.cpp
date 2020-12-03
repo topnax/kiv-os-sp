e0765fe831826da64e74c5b101c56b97637e9984
Stanislav Král <stanislav.kral@smart-software.cz>
2020-12-03 23:03:42 +0100

    Removed the ability to read C:\procfs\tasklist file.

--- proj/src/kernel/proc_fs.cpp
+++ proj/src/kernel/proc_fs.cpp
@@ -12,45 +12,6 @@
 constexpr size_t TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE = 65;
 constexpr int32_t PROCFS_ROOT = 0;
 
-std::vector<char> Proc_Fs::generate_tasklist_vector() {
-    std::vector<char> out;
-
-    // prepare status labels
-    std::string statuses[] = {
-            "Ready", "Running", "Zombie"
-    };
-
-    // generate tasklist header
-    char output[TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE];
-    sprintf_s(output, TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE, "|%-7s |%-10s |%-9s |%-7s |%-7s |%-10s |\n", "PID",
-              "PROGRAM", "STATUS", "STDIN", "STDOUT", "EXIT CODE");
-
-    for (int i = 0; i < TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE - 1; i++) {
-        out.push_back(output[i]);
-    }
-    for (int i = 0; i < TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE - 2; i++) {
-        out.push_back('=');
-    }
-    out.push_back('\n');
-
-    // generate tasklist items
-    for (auto p : Get_Pcb()->Get_Processes()) {
-        // TODO append file attributes
-        sprintf_s(output, TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE, "|%-7d |%-10s |%-9s |%-7d |%-7d |%-10hu |\n", p->handle,
-                  p->program_name,
-                  statuses[(int) p->status].c_str(), p->std_in, p->std_out, p->exit_code);
-
-        for (int i = 0; i < TASK_LIST_PCB_ENTRY_OUT_BUFFER_SIZE - 1; i++) {
-            out.push_back(output[i]);
-        }
-    }
-
-    // null terminated
-    out.push_back(0);
-
-    return out;
-}
-
 std::vector<char> Proc_Fs::generate_readproc_vector(Process p) {
     std::vector<char> out;
 
@@ -68,17 +29,7 @@ kiv_os::NOS_Error Proc_Fs::read(File file, size_t size, size_t offset, std::vect
     // prepare a vector of characters where the generated content will be stored
     std::vector<char> generated;
 
-    if (strcmp(file.name, "\\tasklist") == 0) {
-        // reading tasklist file
-        std::vector<kiv_os::TDir_Entry> entries;
-        auto readdir_result = readdir("", entries);
-
-        if (readdir_result == kiv_os::NOS_Error::Success) {
-            generated = generate_tasklist_vector();
-        } else {
-            return readdir_result;
-        }
-    } else if (strcmp(file.name, "\\") == 0 || strcmp(file.name, "\\.") == 0) {
+    if (strcmp(file.name, "\\") == 0 || strcmp(file.name, "\\.") == 0) {
         // reading procfs directory
         std::vector<kiv_os::TDir_Entry> entries;
         auto readdir_result = readdir("", entries);
@@ -128,20 +79,6 @@ kiv_os::NOS_Error Proc_Fs::readdir(const char *name, std::vector<kiv_os::TDir_En
 kiv_os::NOS_Error Proc_Fs::open(const char *name, uint8_t flags, uint8_t attributes, File &file) {
     std::lock_guard<std::recursive_mutex> guard(*Get_Pcb()->Get_Mutex());
     auto pcb = Get_Pcb();
-    // check whether we are opening tasklist file ore procfs directory
-    if (strcmp(name, "\\tasklist") == 0) {
-        file = File{
-                0,
-                static_cast<uint8_t>(kiv_os::NFile_Attributes::System_File) |
-                static_cast<uint8_t>(kiv_os::NFile_Attributes::Read_Only),
-                sizeof(kiv_os::TDir_Entry::file_name),
-                0,
-                const_cast<char *>(name),
-        };
-        // check attributes
-        if ((attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::System_File)) == 0) return kiv_os::NOS_Error::Permission_Denied;
-        return kiv_os::NOS_Error::Success;
-    }
 
     // check whether opening the root folder - that should print the simplified PCB table
     if (strcmp(name, "\\") == 0 || strcmp(name, "\\.") == 0) {
