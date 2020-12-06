93e22d50c33b6556813964f5ba0d198fb42874eb
Stanislav Král <stanislav.kral@smart-software.cz>
2020-12-06 20:41:07 +0100

    Removed printfs in fatfs

--- proj/src/kernel/fat_fs.cpp
+++ proj/src/kernel/fat_fs.cpp
@@ -5,23 +5,13 @@
 #include "fat_fs.h"
 
 void Fat_Fs::init() {
-
-    printf("################\n");
-    printf("INIT FAT12 disk %d\n", disk_number);
-    printf("param anos %d\n", disk_parameters.absolute_number_of_sectors);
-    printf("param spt %d\n", disk_parameters.sectors_per_track);
-    printf("param bps %d\n", disk_parameters.bytes_per_sector);
-    printf("param c %d\n", disk_parameters.cylinders);
-    printf(" param h %d\n", disk_parameters.heads);
-
-    // init some FAT stuff :D
+    // init FAT
 }
 
 Fat_Fs::Fat_Fs(uint8_t disk_number, kiv_hal::TDrive_Parameters disk_parameters): disk_number(disk_number), disk_parameters(disk_parameters) {}
 
 kiv_os::NOS_Error Fat_Fs::read(File file, size_t size, size_t offset, std::vector<char> &out) {
     // type example.txt
-    printf("tried to read from FAT FS, disk_num=%d, path=%s, size=%d, offset=%d\n", disk_number, file.name, size, offset);
     return kiv_os::NOS_Error::IO_Error;
 }
 
