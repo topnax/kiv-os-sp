77aeb623e6364677fa0f153d608af0c078c4060f
Stanislav Král <stanislav.kral@smart-software.cz>
2020-12-06 21:24:35 +0100

    Fixed error_handler.cpp warnings

--- proj/src/user/error_handler.cpp
+++ proj/src/user/error_handler.cpp
@@ -28,7 +28,7 @@ void handle_error_message(kiv_os::NOS_Error error, kiv_os::THandle std_out) {
 
         default: {
             char error_message[42];
-            sprintf_s(error_message, 42, "Unknown error %d.\n", error);
+            sprintf_s(error_message, 42, "Unknown error %u.\n", error);
             size_t written;
             kiv_os_rtl::Write_File(std_out, error_message, strlen(error_message), written);
             break;
