ef7d74af42a58e2263e02b937a2312da7e96005e
Stanislav Král <stanislav.kral@smart-software.cz>
2020-12-13 20:52:46 +0100

    rd and md programs now do not execute when passed file path is empty

--- proj/src/user/md.cpp
+++ proj/src/user/md.cpp
@@ -15,24 +15,30 @@ extern "C" size_t __stdcall md(const kiv_hal::TRegisters &regs) {
     // load the directory name from rdi register
     char *directory_name = reinterpret_cast<char *>(regs.rdi.r);
 
-    // do not set NOpen_File flag - the file has to be created
-    auto flags = static_cast<kiv_os::NOpen_File>(0);
-
-    // set the file type to directory
-    auto attributes = static_cast<uint8_t>(kiv_os::NFile_Attributes::Directory);
-
-    kiv_os::THandle handle;
-    kiv_os::NOS_Error error;
-    if (kiv_os_rtl::Open_File(directory_name, flags, attributes, handle, error)) {
-        // directory created, close the handle
-        kiv_os_rtl::Close_Handle(handle);
+    if (directory_name == nullptr || strlen(directory_name) == 0) {
+        const char *error_message = "Name not specified.\n";
+        size_t written;
+        kiv_os_rtl::Write_File(std_out, error_message, strlen(error_message), written);
     } else {
-        if (error == kiv_os::NOS_Error::Invalid_Argument) {
-            const char *error_message = "File already exists.\n";
-            size_t written;
-            kiv_os_rtl::Write_File(std_out, error_message, strlen(error_message), written);
+        // do not set NOpen_File flag - the file has to be created
+        auto flags = static_cast<kiv_os::NOpen_File>(0);
+
+        // set the file type to directory
+        auto attributes = static_cast<uint8_t>(kiv_os::NFile_Attributes::Directory);
+
+        kiv_os::THandle handle;
+        kiv_os::NOS_Error error;
+        if (kiv_os_rtl::Open_File(directory_name, flags, attributes, handle, error)) {
+            // directory created, close the handle
+            kiv_os_rtl::Close_Handle(handle);
         } else {
-            handle_error_message(error, std_out);
+            if (error == kiv_os::NOS_Error::Invalid_Argument) {
+                const char *error_message = "File already exists.\n";
+                size_t written;
+                kiv_os_rtl::Write_File(std_out, error_message, strlen(error_message), written);
+            } else {
+                handle_error_message(error, std_out);
+            }
         }
     }
 
